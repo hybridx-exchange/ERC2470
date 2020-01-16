b890852e70eb20a47160322cfb8b9de719195d91
Ricardo Guilherme Schmidt <ricardo3@status.im>
2020-01-16 03:36:12 -0200

    allow salt

--- contracts/SingletonFactory.sol
+++ contracts/SingletonFactory.sol
@@ -7,18 +7,19 @@ pragma solidity 0.5.11;
  */
 contract SingletonFactory {
     /**
-     * @notice Deploys a deterministic address based on `_initCode`.
+     * @notice Deploys a deterministic address based on `_initCode` and `_salt`.
      * @param _initCode Initialization code.
+     * @param _salt Arbitrary value to modify resulting address.
      * @return Created contract address.
      */
-    function deploy(bytes memory _initCode)
+    function deploy(bytes memory _initCode, bytes32 _salt)
         public
         returns (address payable createdContract)
     {
         assembly {
-            createdContract := create2(0, add(_initCode, 0x20), mload(_initCode), 0)
+            createdContract := create2(0, add(_initCode, 0x20), mload(_initCode), _salt)
         }
     }
 }
 // IV is value needed to have a vanity address starting with '0x2470'.
-// IV: 21279
+// IV: 3018
