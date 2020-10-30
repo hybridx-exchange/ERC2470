191c952a2018ae01120455fa2e168d1a7eb90850
Ricardo Guilherme Schmidt <ricardo3@status.im>
2020-10-30 04:21:23 -0300

    update eip and gas needed

--- js/rawTransaction.js
+++ js/rawTransaction.js
@@ -5,7 +5,7 @@ const rawTransaction = {
   gasPrice: 100000000000,
   value: 0,
   data: '0x' + contracts.ERC2470.ERC2470.bin,
-  gasLimit: 247000,
+  gasLimit: 270000,
   v: 27,
   r: '0x247000',
   s: '0x2470'
